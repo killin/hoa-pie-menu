2da8094fd2f3e585bbecf21bdb7b75428760918c
oliverde8@gmail.com <oliverde8@gmail.com@3f78d411-c6fe-640a-f868-9534374e9cac>
2011-05-09 09:20:42 +0000

    -Normalement un PieMenu recursif. Mais le sous Menu ne veut pas s'ouvrir xd je suis perdus

--- trunk/fr/hoa/pie_menu/recursif/Item.java
+++ trunk/fr/hoa/pie_menu/recursif/Item.java
@@ -1,5 +1,7 @@
 package fr.hoa.pie_menu.recursif;
 
+import fr.lri.swingstates.animations.Animation;
+import fr.lri.swingstates.animations.AnimationScaleTo;
 import fr.lri.swingstates.canvas.CPolyLine;
 import fr.lri.swingstates.canvas.CText;
 import fr.lri.swingstates.canvas.Canvas;
@@ -10,7 +12,7 @@ import java.awt.Paint;
 
 /**
  *
- * @author Amirouche HALFAOUI
+ * @author De Cramer Oliver
  */
 public class Item extends CPolyLine {
 	
@@ -25,10 +27,23 @@ public class Item extends CPolyLine {
 	protected int radiusMin;
 
 	protected Item[] subMenu = null;
+	protected Item parent = null;
 	protected Boolean isSubMenu = false;
 	protected Boolean isSubDrawed = false;
 
 	protected CText text;
+
+	protected Boolean mouseIsIn = false;
+
+	public Item(int id, String label, Color color, Canvas canvas, int level, Item parent) {
+		super(0,0);
+		this.label = label;
+		this.color = color;
+		this.level = level;
+		this.id = id;
+		this.canvas = canvas;
+		this.parent = parent;
+	}
 
 	public Item(int id, String label, Color color, Canvas canvas, int level) {
 		super(0,0);
@@ -57,7 +72,6 @@ public class Item extends CPolyLine {
 
 		System.out.println(radius);
 
-
 		this.setAntialiased(true);
 		this.moveTo(radius * Math.cos(angle/2.0), radius * Math.sin(angle/2.0));
 		this.arcTo(-angle/2.0, angle, radius, radius);
@@ -70,7 +84,7 @@ public class Item extends CPolyLine {
 		Paint fp = new GradientPaint(0, 0, color, radiusMax, 0, color.darker());
 		this.setFillPaint(fp);
 		this.setOutlined(false);
-		this.setDrawable(true).setPickable(true);
+		this.setDrawable(false).setPickable(false);
 		this.addTo(canvas);
 
 		this.addTag("menu").addTag("item");
@@ -85,28 +99,61 @@ public class Item extends CPolyLine {
 		text.setFillPaint(color.darker().darker().darker());
 		text.setAntialiased(true);
 		text.addTag("menu").addTag("label");
-		this.addTag("menu-"+level).addTag("label-"+level);
-		text.setDrawable(true).setPickable(true);
+		text.addTag("menu-"+level).addTag("label-"+level);
+		text.setDrawable(false).setPickable(false);
 		text.addTo(canvas);
 	}
 
+	public void addChilds(Item[] sub){
+		subMenu = sub;
+		isSubMenu = true;
+		isSubDrawed = false;
+	}
+
+	private void drawSubMenu(){
+
+		double angle = Math.PI * 2 / subMenu.length;
+
+		for(int i = 0; i < subMenu.length; i++){
+			subMenu[i].drawIt(i, angle, (radiusMax-radiusMin)+radiusMin, radiusMax);
+		}
+
+		isSubDrawed = true;
+	}
+
+	private void showSubMenu(){
+		// Draws the menu at the mouse position
+		canvas.getTag("item-"+(level+1)).translateTo(0,0);
+		canvas.getTag("menu-"+(level+1)).scaleTo(0);
+		Animation anim = new AnimationScaleTo(1, 1);
+		anim.setLapDuration(200);
+		anim.setFunction(Animation.FUNCTION_SIGMOID);
+		anim.setDelay(1);
+		canvas.getTag("menu-"+(level+1)).setDrawable(true).setPickable(true);
+		canvas.getTag("menu-"+(level+1)).animate(anim);
+	}
 
 	public void onMouseEnter(){
 
 		if(this.isSubMenu){
-			//@todo
+			if(!isSubDrawed){
+				drawSubMenu();
+			}
+			showSubMenu();
 		}
 
 		this.setTransparencyFill((float)0.5);
+		mouseIsIn = true;
 
 	}
 
 	public void onMouseLeave(){
 		if(this.isSubMenu){
 			//@todo
+		}else{
+			this.setTransparencyFill((float)1);
 		}
-
-		this.setTransparencyFill((float)1);
+		mouseIsIn = false;
 	}
 
 
