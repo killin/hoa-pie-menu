cd4b2ff18ea4eae1c599c56c702baf79a94648a0
oliverde8 <oliverde8@gmail.com>
2011-05-09 11:20:21 +0000

    -Le sous menu s'affiche sans problemes. Quelque fois il ne veut pas ce fermer mais je vais corriger.
    !La position du sous Menu n'est pas celle desire :S faut calculer

--- fr/hoa/pie_menu/recursif/Item.java
+++ fr/hoa/pie_menu/recursif/Item.java
@@ -9,35 +9,35 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.GradientPaint;
 import java.awt.Paint;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  *
  * @author De Cramer Oliver
  */
 public class Item extends CPolyLine {
-	
+
 	protected String label;
 	protected Color color;
 	protected Canvas canvas;
-
 	protected int level;
-
 	protected int id;
 	protected int radiusMax;
 	protected int radiusMin;
-
 	protected Item[] subMenu = null;
 	protected Item parent = null;
 	protected Boolean isSubMenu = false;
 	protected Boolean isSubDrawed = false;
 	protected Boolean isSubShowed = false;
-
+	protected Timer timer;
 	protected CText text;
-
 	protected Boolean mouseIsIn = false;
 
 	public Item(int id, String label, Color color, Canvas canvas, int level, Item parent) {
-		super(0,0);
+		super(0, 0);
 		this.label = label;
 		this.color = color;
 		this.level = level;
@@ -47,7 +47,7 @@ public class Item extends CPolyLine {
 	}
 
 	public Item(int id, String label, Color color, Canvas canvas, int level) {
-		super(0,0);
+		super(0, 0);
 		this.label = label;
 		this.color = color;
 		this.level = level;
@@ -56,7 +56,7 @@ public class Item extends CPolyLine {
 	}
 
 	public Item(int id, String label, Color color, Canvas canvas) {
-		super(0,0);
+		super(0, 0);
 		this.label = label;
 		this.color = color;
 		this.level = 1;
@@ -64,29 +64,29 @@ public class Item extends CPolyLine {
 		this.canvas = canvas;
 	}
 
-	public void drawIt(int i, double angle, int radius, int radiusMin){
+	public void drawIt(int i, double angle, int radius, int radiusMin) {
 
 		radiusMax = radius;
 		this.radiusMin = radiusMin;
 		int parentId;
 
-		if(parent == null){
+		if (parent == null) {
 			parentId = 0;
-		}else{
+		} else {
 			parentId = parent.getId();
 		}
 
 		this.rotateTo(i * angle);
 
-		System.out.println(radius+"-"+radiusMin);
+		System.out.println(radius + "-" + radiusMin);
 
 		this.setAntialiased(true);
-		this.moveTo(radius * Math.cos(angle/2.0), radius * Math.sin(angle/2.0));
-		this.arcTo(-angle/2.0, angle, radius, radius);
-		this.lineTo(radiusMin * Math.cos(angle/2.0), -radiusMin * Math.sin(angle/2.0));
-		this.arcTo(angle/2, -angle, radiusMin, radiusMin);
-		this.moveTo(0,0);
-		this.setReferencePoint(0,0.5);
+		this.moveTo(radius * Math.cos(angle / 2.0), radius * Math.sin(angle / 2.0));
+		this.arcTo(-angle / 2.0, angle, radius, radius);
+		this.lineTo(radiusMin * Math.cos(angle / 2.0), -radiusMin * Math.sin(angle / 2.0));
+		this.arcTo(angle / 2, -angle, radiusMin, radiusMin);
+		this.moveTo(0, 0);
+		this.setReferencePoint(0, 0.5);
 
 		// Sets the item's color
 		Paint fp = new GradientPaint(0, 0, color, radiusMax, 0, color.darker());
@@ -96,113 +96,142 @@ public class Item extends CPolyLine {
 		this.addTo(canvas);
 
 		this.addTag("menu").addTag("item");
-		this.addTag("menu-"+parentId).addTag("item-"+parentId);
+		this.addTag("menu-" + parentId).addTag("item-" + parentId);
 
 		// Adds the item's label
 		text = canvas.newText(0, 0, label, new Font(Font.SANS_SERIF, Font.BOLD, 9));
 		text.rotateBy(-i * angle);
-		text.translateTo(radius/1.4, -1);
+		text.translateTo(radius / 1.4, -1);
 		text.setParent(this);
 		text.setClip(this);
 		text.setFillPaint(color.darker().darker().darker());
 		text.setAntialiased(true);
 		text.addTag("menu").addTag("label");
-		text.addTag("menu-"+parentId).addTag("label-"+parentId);
+		text.addTag("menu-" + parentId).addTag("label-" + parentId);
 		text.setDrawable(false).setPickable(false);
 		text.addTo(canvas);
-		
-		
-		if(this.isSubMenu && !isSubDrawed){
+
+
+		if (this.isSubMenu && !isSubDrawed) {
 			drawSubMenu();
 		}
 	}
 
-	public void addChilds(Item[] sub){
+	public void addChilds(Item[] sub) {
 		subMenu = sub;
 		isSubMenu = true;
 		isSubDrawed = false;
 	}
 
-	private void drawSubMenu(){
+	private void drawSubMenu() {
 
 		double angle = Math.PI * 2 / subMenu.length;
 
-		for(int i = 0; i < subMenu.length; i++){
-			subMenu[i].drawIt(i, angle, (radiusMax-radiusMin)+radiusMax, radiusMax);
+		for (int i = 0; i < subMenu.length; i++) {
+			subMenu[i].drawIt(i, angle, (radiusMax - radiusMin) + radiusMax, radiusMax);
 		}
 
 		isSubDrawed = true;
 	}
 
-	private void showSubMenu(){
+	private void showSubMenu() {
 		// Draws the menu at the mouse position
-		canvas.getTag("item-"+id).translateTo(this.getMinX(),this.getMaxY());
-		
-		canvas.getTag("menu-"+id).scaleTo(0);
+		canvas.getTag("item-" + id).translateTo(this.getMinX(), this.getMaxY());
+
+		canvas.getTag("menu-" + id).scaleTo(0.8);
 		Animation anim = new AnimationScaleTo(1, 1);
 		anim.setLapDuration(200);
 		anim.setFunction(Animation.FUNCTION_SIGMOID);
 		anim.setDelay(1);
-		canvas.getTag("menu-"+id).setDrawable(true).setPickable(true);
-		canvas.getTag("menu-"+id).animate(anim);
+		canvas.getTag("menu-" + id).setDrawable(true).setPickable(true);
+		canvas.getTag("menu-" + id).animate(anim);
 	}
 
-	public void closeSubMenus(){
-		if(isSubMenu){
-			canvas.getTag("menu-"+id).setDrawable(false).setPickable(false);
-			for(int i = 0; i < subMenu.length; i++){
+	public void closeSubMenus() {
+		if (isSubMenu) {
+			timer = null;
+
+			canvas.getTag("menu-" + id).setDrawable(false).setPickable(false);
+
+			canvas.getTag("menu-" + id).scaleTo(1);
+			Animation anim = new AnimationScaleTo(0.8, 0.8);
+			anim.setLapDuration(200);
+			anim.setFunction(Animation.FUNCTION_SIGMOID);
+			anim.setDelay(1);
+			canvas.getTag("menu-" + id).setDrawable(false).setPickable(false);
+			canvas.getTag("menu-" + id).animate(anim);
+
+			for (int i = 0; i < subMenu.length; i++) {
 				subMenu[i].closeSubMenus();
 			}
 			isSubShowed = false;
 		}
 	}
 
-	public void onMouseEnter(){
+	public void onMouseEnter() {
 
-		if(!mouseIsIn){
-			if(this.isSubMenu && !isSubShowed){
+		if (!mouseIsIn) {
+			if (this.isSubMenu && !isSubShowed) {
 
-				showSubMenu();
+				if(timer == null)
+					showSubMenu();
+				else{
+					timer.cancel();
+					timer = null;
+				}
+
 				isSubShowed = true;
 			}
 
-			this.setTransparencyFill((float)0.5);
+			this.setTransparencyFill((float) 0.5);
 			mouseIsIn = true;
 		}
-
 	}
 
-	public void onMouseLeave(){
-		if(mouseIsIn){
-			mouseIsIn = false;
-			if(this.isSubMenu){
+	public void onMouseLeave() {
+		if (mouseIsIn) {
 
-				if(!getMouseIsIn()){
-					this.setTransparencyFill((float)1);
-					System.out.println("close");
-					//closeSubMenus();
+			mouseIsIn = false;
+			if (this.isSubMenu) {
+
+				if (!getMouseIsIn()) {
+					
+					timer = new Timer();
+					TimerTask task = new TimerTask() {
+
+						@Override
+						public void run() {
+							if(!getMouseIsIn()){
+								closeSubMenus();
+								setTransparencyFill((float) 1);
+							}
+						}
+					};
+					timer.schedule(task, (long)100);
 				}
 
-			}else{
-				this.setTransparencyFill((float)1);
+			} else {
+				this.setTransparencyFill((float) 1);
 			}
 		}
 	}
 
-	public int getId(){
+	public int getId() {
 		return id;
 	}
 
 	public Boolean getMouseIsIn() {
-		if(mouseIsIn)
+		if (mouseIsIn) {
 			return true;
-		else if(isSubMenu){
-			for(int i = 0; i < subMenu.length; i++){
-				if(subMenu[i].getMouseIsIn())
+		} else if (isSubMenu) {
+			for (int i = 0; i < subMenu.length; i++) {
+				if (subMenu[i].getMouseIsIn()) {
 					return true;
+				}
 			}
 			return false;
-		}else
+		} else {
 			return false;
+		}
 	}
 }
