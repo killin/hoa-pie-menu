740cb784df64fe9f8c4b88ad068ef6690847a354
oliverde8 <oliverde8@gmail.com>
2011-05-11 18:11:24 +0000

    -Modification de Application2 pour ajputer des sous sous Menu pour que ça fasse classe

--- fr/hoa/pie_menu/Application2.java
+++ fr/hoa/pie_menu/Application2.java
@@ -39,17 +39,17 @@ public class Application2 extends JFrame {
 			};
 
 			for(int i = 0; i < colors.length-3; i++){
-				items[i] = new Item(i+1, labels[i], colors[i], canvas);
+				items[i] = new Item(labels[i], colors[i], canvas);
 				items[i].setActionListener(colorChange);
 			}
 
-			items[colors.length-3] = subMenu(colors.length, colors[colors.length-3], colorChange);
+			items[colors.length-3] = subMenu(colors[colors.length-3], colorChange, 2, null);
 			items[colors.length-3].setStyle(Item.STYLE_CERCLES);
 
-			items[colors.length-2] = subMenu(colors.length+10, colors[colors.length-2], colorChange);
+			items[colors.length-2] = subMenu(colors[colors.length-2], colorChange, 2, null);
 			items[colors.length-2].setStyle(Item.STYLE_TRANCHE);
 
-			items[colors.length-1] = subMenu(colors.length+20, colors[colors.length-1], colorChange);
+			items[colors.length-1] = subMenu2(colors[colors.length-1], colorChange);
 			
 			menu = new Menu(canvas, items);
 			this.add(canvas);
@@ -58,18 +58,43 @@ public class Application2 extends JFrame {
 			this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
-	private Item subMenu(int i, Color c, ActionListener colorChange){
+	private Item subMenu(Color c, ActionListener colorChange, int level, Item parent){
 		final Color[] colors = { Color.MAGENTA, Color.BLUE, Color.CYAN, Color.GREEN, Color.YELLOW, Color.ORANGE};
 		final String[] labels = {"Violet", "Bleu", "Cyan", "Vert", "Jaune", "Orange"};
 		final Item[] items = new Item[labels.length];
 
-		Item smenu = new Item(i, "Sub Menu", c, canvas);
+		Item smenu = new Item("Sub Menu", c, canvas, level-1, parent);
 
 		for(int cpt = 0; cpt < colors.length; cpt++){
-			items[cpt] = new Item(cpt+i+1, labels[cpt], colors[cpt], canvas, 2, smenu);
+			items[cpt] = new Item(labels[cpt], colors[cpt], canvas, level, smenu);
 			items[cpt].setActionListener(colorChange);
 		}
 
+		smenu.addChilds(items);
+
+		return smenu;
+	}
+
+	private Item subMenu2(Color c, ActionListener colorChange){
+		final Color[] colors = { Color.MAGENTA, Color.BLUE, Color.CYAN, Color.GREEN, Color.YELLOW, Color.ORANGE};
+		final String[] labels = {"Violet", "Bleu", "Cyan", "Vert", "Jaune", "Orange"};
+		final Item[] items = new Item[labels.length];
+
+		Item smenu = new Item("Sub Menu", c, canvas);
+
+		for(int cpt = 0; cpt < colors.length-3; cpt++){
+			items[cpt] = new Item(labels[cpt], colors[cpt], canvas, 2, smenu);
+			items[cpt].setActionListener(colorChange);
+		}
+
+		items[colors.length-3] = subMenu(colors[colors.length-3], colorChange, 3, smenu);
+		items[colors.length-3].setStyle(Item.STYLE_CERCLES);
+
+		items[colors.length-2] = subMenu(colors[colors.length-2], colorChange, 3, smenu);
+		items[colors.length-2].setStyle(Item.STYLE_TRANCHE);
+
+		items[colors.length-1] = subMenu(colors[colors.length-1], colorChange,3, smenu);
+		
 		smenu.addChilds(items);
 
 		return smenu;
